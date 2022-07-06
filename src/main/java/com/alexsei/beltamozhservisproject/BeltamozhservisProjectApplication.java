53e0795625f0e00f5034b303ffac4322dc9ff05e
Alexsei <repyls2@mail.ru>
2022-07-06 15:54:27 +0300

    Initial commit

---
+++ src/main/java/com/alexsei/beltamozhservisproject/BeltamozhservisProjectApplication.java
@@ -0,0 +1,13 @@
+package com.alexsei.beltamozhservisproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeltamozhservisProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeltamozhservisProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alexsei/beltamozhservisproject/BeltamozhservisProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexsei.beltamozhservisproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeltamozhservisProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
