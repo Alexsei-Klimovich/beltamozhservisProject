9d9ad91940b543cefee9077910ec7db58f79f341
Yaroslav <yarikshtykh@gmail.com>
2022-07-07 16:50:33 +0300

    add JpaRepositoryes

---
+++ src/main/java/by/btslogistics/beltamozhservisproject/repository/GrafaRepository.java
@@ -0,0 +1,8 @@
+package by.btslogistics.beltamozhservisproject.repository;
+
+import by.btslogistics.beltamozhservisproject.model.Grafa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GrafaRepository extends JpaRepository<Long, Grafa> {
+
+}
