7c27c7f7ffdd9d5053ae654b49564e784996bfe1
Yaroslav <yarikshtykh@gmail.com>
2022-07-07 15:58:24 +0300

    add dao

--- src/main/java/by/btslogistics/beltamozhservisproject/model/Document.java
+++ src/main/java/by/btslogistics/beltamozhservisproject/model/Document.java
@@ -1,5 +1,34 @@
 package by.btslogistics.beltamozhservisproject.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "STRUCTURE_DOCUMENT")
+@Entity
 public class Document {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "schema_location")
+    private String schemaLocation;
+
+    @Column(name = "root_element")
+    private String rootElement;
+
+    @Column(name = "schema_version")
+    private String schemaVersion;
+
+    @Column(name = "schema_name")
+    private String schemaName;
 }
--- src/main/java/by/btslogistics/beltamozhservisproject/model/Tag.java
+++ src/main/java/by/btslogistics/beltamozhservisproject/model/Tag.java
@@ -1,5 +1,43 @@
 package by.btslogistics.beltamozhservisproject.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "TAG_DOCUMENT")
 public class Tag {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "to_strdoc_id")
+    private Long toStrdocId;
+
+    @Column(name = "node_name")
+    private Long nodeName;
+
+    @Column(name = "node_path")
+    private String nodePath;
+
+    @Column(name = "parent_name")
+    private String parentName;
+
+    @Column(name = "parent_path")
+    private String parentPath;
+
+    @Column(name = "pattern")
+    private String pattern;
+
+    @Column(name = "pattern_id")
+    private Long patternId;
 }
