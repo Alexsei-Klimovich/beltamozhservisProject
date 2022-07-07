9cdd8e0c1d46a1b93a0609696036de352604099c
Alexsei <repyls2@mail.ru>
2022-07-07 16:58:55 +0300

    Add TagRepository

---
+++ src/main/java/by/btslogistics/beltamozhservisproject/repository/TagRepository.java
@@ -0,0 +1,7 @@
+package by.btslogistics.beltamozhservisproject.repository;
+
+import by.btslogistics.beltamozhservisproject.model.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Long, Tag> {
+}
