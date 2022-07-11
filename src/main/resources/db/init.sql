bb648e93785bdcdd4eba28680668cd498ba56bd8
Yaroslav <yarikshtykh@gmail.com>
2022-07-11 09:52:01 +0300

    added sequences for all tables

--- src/main/resources/db/init.sql
+++ src/main/resources/db/init.sql
@@ -2,7 +2,13 @@ drop table if exists FLK_GRAFA cascade;
 drop table if exists STRUCTURE_DOCUMENT CASCADE;
 drop table if exists TAG_DOCUMENT CASCADE;
 drop table if exists FLK_CHECKS CASCADE ;
+drop sequence if exists seq_flk_checks cascade;
+drop sequence if exists seq_flk_grafa cascade;
+drop sequence if exists seq_tag_document cascade;
+drop sequence if exists seq_structure_document cascade;
 
+
+create sequence if not exists seq_flk_checks start 100000;
 create table FLK_CHECKS (
                             ID numeric(19) PRIMARY KEY,
                             ID_GRAFA numeric(19) not null,
@@ -14,14 +20,16 @@ create table FLK_CHECKS (
                             D_OFF date not null
 );
 
+create sequence if not exists seq_flk_grafa start 100000;
 create table FLK_GRAFA (
                            ID numeric(19) primary key,
                            ID_FORM numeric(20) not null,
                            NAME_GRAFA varchar(300) not null,
                            NAME_POLE varchar(300) not null,
-                           PATH_XML varchar(500) not null,
+                           PATH_XML varchar(500) not null
 );
 
+create sequence if not exists seq_tag_document start 100000;
 create table TAG_DOCUMENT (
                               ID numeric(19) primary key,
                               TO_STRDOC_ID numeric(19) not null,
@@ -30,16 +38,22 @@ create table TAG_DOCUMENT (
                               PARENT_NAME varchar(700),
                               PARENT_PATH varchar(700),
                               PATTERN varchar(255),
-                              PARENT_ID numeric(19),
+                              PARENT_ID numeric(19)
 );
 
+create sequence if not exists seq_structure_document start 100000;
 create table STRUCTURE_DOCUMENT (
                                     ID numeric(19) primary key,
                                     SCHEMA_LOCATION varchar(700) not null,
                                     ROOT_ELEMENT varchar(700) not null,
                                     SCHEMA_VERSION varchar(50) not null,
-                                    SCHEMA_NAME varchar(50) not null,
+                                    SCHEMA_NAME varchar(50) not null
 );
+
+ALTER TABLE FLK_CHECKS ALTER COLUMN ID SET DEFAULT nextval('seq_flk_checks');
+ALTER TABLE FLK_GRAFA ALTER COLUMN ID SET DEFAULT nextval('seq_flk_grafa');
+ALTER TABLE TAG_DOCUMENT ALTER COLUMN ID SET DEFAULT nextval('seq_tag_document');
+ALTER TABLE STRUCTURE_DOCUMENT ALTER COLUMN ID SET DEFAULT nextval('seq_structure_document');
 
 ALTER table FLK_CHECKS  ADD constraint FK_CHECKS_GRAFA_ID foreign key (ID_GRAFA)
       references FLK_GRAFA (ID)
