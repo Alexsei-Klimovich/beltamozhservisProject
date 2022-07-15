80532545f31ae6f06daade1b97165913bc43e745
Yaroslav <yarikshtykh@gmail.com>
2022-07-15 12:05:54 +0300

    -add dependencies -delete not used classes -refactor XsdService

--- src/main/java/by/btslogistics/beltamozhservisproject/parser/xsd/XsdParser.java
+++ src/main/java/by/btslogistics/beltamozhservisproject/parser/xsd/XsdParser.java
@@ -1,16 +1,12 @@
 package by.btslogistics.beltamozhservisproject.parser.xsd;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
-
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+
 
 public class XsdParser {
 
--- src/main/java/by/btslogistics/beltamozhservisproject/service/XsdService.java
+++ src/main/java/by/btslogistics/beltamozhservisproject/service/XsdService.java
@@ -1,201 +1,95 @@
 package by.btslogistics.beltamozhservisproject.service;
 
-import by.btslogistics.beltamozhservisproject.model.Grafa;
-import by.btslogistics.beltamozhservisproject.model.StructureDocument;
-
-import by.btslogistics.beltamozhservisproject.parser.xsd.XsdParser;
+import org.apache.ws.commons.schema.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
-import java.io.IOException;
+import javax.xml.bind.annotation.XmlSchema;
+import javax.xml.namespace.QName;
+import javax.xml.transform.stream.StreamSource;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-import static by.btslogistics.beltamozhservisproject.parser.xsd.XsdParser.buildDocumentFromFile;
 
 @Service
 public class XsdService {
+    private XmlSchemaCollection xmlSchemaCollection;
+    private static Map<QName, List<XmlSchemaElement>> xsdElements = new HashMap<>();
+    private List<XmlSchemaElement> schemaElements = new ArrayList<>();
+
     @Autowired
     StructureDocumentService structureDocumentService;
 
     @Autowired
     GrafaService grafaService;
 
+    public void xsdParse() throws URISyntaxException, FileNotFoundException {
+        String xsdPath = Paths.get
+                (XsdService.class.getClassLoader().
+                        getResource("C:\\Users\\0_shtykh_ya\\IdeaProjects\\beltamozhservisProject\\EEC_M_CA_SimpleDataObjects_vbts2.xsd")
+                        .toURI()).toFile().getAbsolutePath();
+        String filePath = Path.of(xsdPath).toString();
 
+        InputStream inputStream = new FileInputStream(filePath);
+        xmlSchemaCollection = new XmlSchemaCollection();
 
-    public void saveRootXsd(File file) throws IOException, ParserConfigurationException, SAXException {
-        StructureDocument structureDocument = new StructureDocument();
-        Document document = buildDocumentFromFile(file);
-        NodeList elements = document.getElementsByTagName("xs:element");
-        NodeList schemas = document.getElementsByTagName("xs:schema");
-        Element schema =(Element) schemas.item(0);
-        String version = schema.getAttribute("version");
-        for (int i = 0; i < elements.getLength(); i++) {
-            Element element = (Element) elements.item(i);
-            if(element.hasAttribute("type")){
-                String rootElement = element.getAttribute("type").replace("Type","");
-                String schemaName = element.getAttribute("name");
-                String schemaLocation = file.getPath();
-                System.out.println();
-                structureDocument.setRootElement(rootElement);
-                structureDocument.setSchemaLocation(schemaLocation);
-                structureDocument.setSchemaVersion(version);
-                structureDocument.setSchemaName(schemaName);
-                structureDocumentService.saveStructureDocument(structureDocument);
-            }
-        }
-    }
-    //TODO CREATE CHECK FOR FIELD EXISTS IN DB ADD PREFIX /SOMETHING  TO XML PATH
-    public void saveFlkGrafa(File file) throws IOException, ParserConfigurationException, SAXException {
-        Document document = XsdParser.buildDocumentFromFile(file);
-        NodeList documentations = document.getElementsByTagName("xs:documentation");
-        NodeList schemas = document.getElementsByTagName("xs:schema");
-        Element schema =(Element) schemas.item(0);
-        ///TODO CREATE METHOD TO SLIT ATTR PREFIX
-        NamedNodeMap preTag = schema.getAttributes();
-        String attr = String.valueOf(preTag.item(4));
-        List<String> atrr = List.of(attr.split("="));
-        List<String> splitedAttr = List.of(atrr.get(0).split(":"));
-        String prefixAttribute = splitedAttr.get(1);
-        /////
-        String prefixPathXml  = "/";
-        prefixPathXml+= prefixAttribute;
-        prefixPathXml+=":";
+        XmlSchema schema = (XmlSchema) new XmlSchemaCollection().read(new StreamSource(inputStream));
+        Map.Entry<QName, XmlSchemaElement> entry = (Map.Entry<QName, XmlSchemaElement>) schema;
+        QName rootElement = entry.getKey();
 
-        for (int i = 0; i < documentations.getLength(); i++) {
-            Grafa grafa = new Grafa();
-            String pathXml = "";
+        XmlSchemaElement childElement = xmlSchemaCollection.getElementByQName(rootElement);
 
-            Element element = (Element) documentations.item(i);
-            Element parentElement = (Element) element.getParentNode().getParentNode();
-            String attributeName = parentElement.getAttribute("name");
-            String parentAttributeRef = parentElement.getAttribute("ref");
-
-            if(attributeName.isEmpty()&& parentAttributeRef.isEmpty()){
-                Element doubleParent = (Element) parentElement.getParentNode().getParentNode();
-                String attributeRef = doubleParent.getAttribute("ref");
-                pathXml = prefixPathXml+attributeRef;
-
-            } else if(parentAttributeRef.isEmpty())
-            {
-                pathXml = prefixPathXml+attributeName;
-
-            }else {
-                pathXml = "/"+parentAttributeRef;
-            }
-            String formId = "UPDT";
-            String nameGrafa=element.getTextContent();
-            String namePole=element.getTextContent();
-
-            System.out.println();
-            grafa.setFormId(0L);//TODO String to Long parameter in entity
-            grafa.setPathXML(pathXml);
-            grafa.setNameGrafa(nameGrafa);
-            grafa.setNamePole(namePole);
-            grafaService.saveGrafa(grafa);
-        }
+        getChildElementNames(childElement);
+        String element = "" + xsdElements.entrySet().stream().map(e -> e.getKey() + " -- " + String.join(", ", e.getValue().stream().map(v -> v
+                .getQName().toString()).collect(Collectors.toList()))).collect(Collectors.toList());
     }
 
-    public void printFlkGrafaInfo(File file) throws IOException, ParserConfigurationException, SAXException {
-        Document document = XsdParser.buildDocumentFromFile(file);
-        NodeList documentations = document.getElementsByTagName("xs:documentation");
-        NodeList schemas = document.getElementsByTagName("xs:schema");
-        Element schema =(Element) schemas.item(0);
+    private void getChildElementNames(XmlSchemaElement element) {
+        XmlSchemaType elementType = element != null ? element.getSchemaType() : null;
 
-        ///TODO CREATE METHOD TO SLIT ATTR PREFIX
-        NamedNodeMap preTag = schema.getAttributes();
-        String attr = String.valueOf(preTag.item(4));
-        List<String> atrr = List.of(attr.split("="));
-        List<String> splitedAttr = List.of(atrr.get(0).split(":"));
-        String prefixAttribute = splitedAttr.get(1);
-        /////
+        if (elementType instanceof XmlSchemaComplexType) {
+            XmlSchemaParticle allParticles = ((XmlSchemaComplexType) elementType).getParticle();
 
+            if (allParticles instanceof XmlSchemaAny) {
+                System.out.println("Any Schema Type");
 
-        String prefixPathXml  = "/";
-        prefixPathXml+= prefixAttribute;
-        prefixPathXml+=":";
+            } else if (allParticles instanceof XmlSchemaElement) {
+                System.out.println("Element Schema Type");
 
+            } else if (allParticles instanceof XmlSchemaSequence) {
+                final XmlSchemaSequence xmlSchemaSequence = (XmlSchemaSequence) allParticles;
+                final List<XmlSchemaSequenceMember> items = xmlSchemaSequence.getItems();
+                items.forEach((item) -> {
+                    XmlSchemaElement itemElements = (XmlSchemaElement) item;
+                    schemaElements.add(itemElements);
 
+                    addChild(element.getQName(), itemElements);
+                    getChildElementNames(itemElements);
+                    schemaElements = new ArrayList<XmlSchemaElement>();
+                });
 
-        for (int i = 0; i < documentations.getLength(); i++) {
-            String pathXml = "";
+            } else if (allParticles instanceof XmlSchemaGroupRef) {
 
-            Element element = (Element) documentations.item(i);
-            Element parentElement = (Element) element.getParentNode().getParentNode();
-            String attributeName = parentElement.getAttribute("name");
-            String parentAttributeRef = parentElement.getAttribute("ref");
-
-            if(attributeName.isEmpty()&& parentAttributeRef.isEmpty()){
-                Element doubleParent = (Element) parentElement.getParentNode().getParentNode();
-                String attributeRef = doubleParent.getAttribute("ref");
-                pathXml = prefixPathXml+attributeRef;
-
-            } else if(parentAttributeRef.isEmpty())
-            {
-                    pathXml = prefixPathXml+attributeName;
-
-            }else {
-                pathXml = "/"+parentAttributeRef;
-            }
-            String formId = "UPDT";
-            String nameGrafa=element.getTextContent();
-            String namePole=element.getTextContent();
-
-            System.out.println();
-            System.out.println("ID_FORM:"+formId);
-            System.out.println("NAME_GRAFA:"+nameGrafa);
-            System.out.println("NAME_POLE:"+namePole);
-            System.out.println("PATH_XML:"+pathXml);
-        }
-    }
-    public void printRootXsdInfo(File file) throws IOException, ParserConfigurationException, SAXException {
-        Document document = buildDocumentFromFile(file);
-        NodeList elements = document.getElementsByTagName("xs:element");
-        NodeList schemas = document.getElementsByTagName("xs:schema");
-        Element schema =(Element) schemas.item(0);
-        String version = schema.getAttribute("version");
-        for (int i = 0; i < elements.getLength(); i++) {
-            Element element = (Element) elements.item(i);
-            if(element.hasAttribute("type")){
-                String rootElement = element.getAttribute("type").replace("Type","");
-                String schemaName = element.getAttribute("name");
-                String schemaLocation = file.getPath();
-                System.out.println();
-                System.out.println("SCHEMA_LOCATION:"+schemaLocation);
-                System.out.println("ROOT_ELEMENT:"+rootElement);
-                System.out.println("SCHEMA_VERSION:"+ version);
-                System.out.println("SCHEMA_NAME:"+ schemaName);
             }
         }
+
     }
 
-
-
-    public static void printXsdInfo(Document document) {
-        NodeList documentations = document.getElementsByTagName("xs:documentation");
-        System.out.println("__________________________________");
-        for (int i = 0; i < documentations.getLength(); i++) {
-            Element element = (Element) documentations.item(i);
-            Element parentElement = (Element) element.getParentNode().getParentNode();
-            if (parentElement.getAttribute("name").isEmpty()) {
-                Element doubleParentNode = (Element) parentElement.getParentNode().getParentNode();
-                System.out.println("name: " + doubleParentNode.getAttribute("name"));
-                System.out.println("type: " + doubleParentNode.getAttribute("type"));
-                System.out.println("parent: " + parentElement.getParentNode().getParentNode().getNodeName());
-            } else {
-                System.out.println("name: " + parentElement.getAttribute("name"));
-                System.out.println("type: " + parentElement.getAttribute("type"));
-                System.out.println("parent: " + element.getParentNode().getParentNode().getNodeName());
-            }
-            System.out.println("documentation: " + element.getTextContent());
-            System.out.println("__________________________________");
+    public static void addChild(QName qName, XmlSchemaElement child) {
+        List<XmlSchemaElement> values = xsdElements.get(qName);
+        if (values == null) {
+            values = new ArrayList<XmlSchemaElement>();
         }
+        values.add(child);
+        xsdElements.put(qName, values);
     }
-
 }
