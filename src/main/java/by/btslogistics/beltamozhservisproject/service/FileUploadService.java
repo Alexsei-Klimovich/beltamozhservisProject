87cd944e54bb0539fa0ed9abff668afe6e85d607
Yaroslav <yarikshtykh@gmail.com>
2022-07-21 15:24:37 +0300

    bug fix with upload files

--- src/main/java/by/btslogistics/beltamozhservisproject/service/FileUploadService.java
+++ src/main/java/by/btslogistics/beltamozhservisproject/service/FileUploadService.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.xml.sax.SAXException;
-
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
@@ -23,7 +22,7 @@ public class FileUploadService {
     @Autowired
     XmlService xmlService;
     private List<String> splitedOriginalName = new ArrayList<>();
-    private String destenationPath = "C:\\Users\\0_shtykh_ya\\IdeaProjects\\beltamozhservisProject\\uploadsFiles";
+    private String destenationPath = "uploadsFiles";
     private Path path = Path.of(destenationPath);
     private String fileExtension;
     private String fileName;
@@ -32,23 +31,21 @@ public class FileUploadService {
         if (!Files.exists(path)) {
             Files.createDirectory(path);
         }
-
-            System.out.println(multipartFile.getOriginalFilename());
-            fileName = multipartFile.getOriginalFilename();
-            filePath = destenationPath + "\\" + fileName;
-            fileExtension = FileNameUtils.getExtension(multipartFile.getOriginalFilename());
-            splitedOriginalName.add(fileExtension);
-            System.out.println(splitedOriginalName.get(splitedOriginalName.lastIndexOf(fileExtension)));
-            if (fileExtension.equals("xlsx") || fileExtension.equals("xls")) {
-                multipartFile.transferTo(new File(filePath));
-//                excelParse.excelParse(new File(filePath));
-            } else if (fileExtension.equals("xsd")) {
-                multipartFile.transferTo(new File(filePath));
-//                xmlService.saveDocumentInfo(new File(filePath));
-            } else {
-                throw new InvalidFileTypeException();
-            }
-
+        System.out.println(multipartFile.getOriginalFilename());
+        fileName = multipartFile.getOriginalFilename();
+        filePath = destenationPath + "\\" + fileName;
+        fileExtension = FileNameUtils.getExtension(multipartFile.getOriginalFilename()); // получаем расширение файла
+        splitedOriginalName.add(fileExtension);
+        System.out.println(splitedOriginalName.get(splitedOriginalName.lastIndexOf(fileExtension)));
+        if (fileExtension.equals("xlsx") || fileExtension.equals("xls")) { // проверка на тип файла
+            multipartFile.transferTo(new File(filePath));
+//          excelParse.excelParse(new File(filePath));
+        } else if (fileExtension.equals("xsd")) {
+            multipartFile.transferTo(new File(filePath));
+//          xmlService.saveDocumentInfo(new File(filePath));
+        } else {
+          throw new InvalidFileTypeException();
+        }
         return "File uploaded";
     }
 }
