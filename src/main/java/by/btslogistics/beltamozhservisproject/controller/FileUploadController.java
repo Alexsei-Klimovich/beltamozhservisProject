221e8dd49754a49640d17a34893ec838a614b262
Yaroslav <yarikshtykh@gmail.com>
2022-07-27 12:47:23 +0300

    add GetMapping for transfer files names to front

--- src/main/java/by/btslogistics/beltamozhservisproject/controller/FileUploadController.java
+++ src/main/java/by/btslogistics/beltamozhservisproject/controller/FileUploadController.java
@@ -1,7 +1,10 @@
 package by.btslogistics.beltamozhservisproject.controller;
 import by.btslogistics.beltamozhservisproject.service.FileUploadService;
+import org.apache.commons.compress.utils.FileNameUtils;
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +12,20 @@ import org.springframework.web.multipart.MultipartFile;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.aspectj.util.FileUtil.listFiles;
+
 @RestController
 public class FileUploadController {
     @Autowired
     FileUploadService fileUploadService;
     private byte[] content = null;
     private MultipartFile uploadedFile;
+
     @PostMapping("/upload")
     public String fileUpload(@RequestParam("file")MultipartFile[] multipartFile) throws IOException, ParserConfigurationException, SAXException {
         for (int i = 0; i < multipartFile.length; i++) {
@@ -37,5 +47,22 @@ public class FileUploadController {
             }
         }
         return "File uploaded";
+    }
+
+    @GetMapping("/getFilesNames")
+    public List<String> getFilesNames() throws IOException {
+        List<String> listFilesNames = new ArrayList<>();
+        File folder = new File("uploadsFiles\\");
+        for (File file : folder.listFiles()) {
+            if (file.isDirectory()) {
+                listFiles(file);
+            } else {
+                String fileExtension = FileNameUtils.getExtension(String.valueOf(file));
+                if (fileExtension.equals("xlsx") || fileExtension.equals("xls")) {
+                    listFilesNames.add(FilenameUtils.getName(String.valueOf(file)));
+                }
+            }
+        }
+        return listFilesNames;
     }
 }
