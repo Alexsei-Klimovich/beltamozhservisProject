532831f534600ab12521d0eeca01bb33575673e5
y.shtykh <LopkA_02>
2023-01-03 17:21:52 +0300

    add replacing old code_check on new code_check from directory

---
+++ src/main/java/by/btslogistics/beltamozhservisproject/service/ChangeCodeCheckService.java
@@ -0,0 +1,21 @@
+package by.btslogistics.beltamozhservisproject.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.util.Map;
+
+@Service
+public class ChangeCodeCheckService {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(ChangeCodeCheckService.class);
+
+    public void replaceCodeCheck(File excelFile) {
+        Map<String, String> checks = ExcelService.oldAndNewChecksFromExcel(excelFile);
+        for (Map.Entry<String, String> check: checks.entrySet()) {
+            LOGGER.info(check.getKey() + " : " + check.getValue());
+        }
+    }
+}
