23e5bcfeb7f7e50dc7c188a7af5775d6dbb6b4a1
y.shtykh <LopkA_02>
2023-01-04 12:58:00 +0300

    add service for save files

---
+++ src/main/java/by/btslogistics/beltamozhservisproject/service/SaveFileService.java
@@ -0,0 +1,28 @@
+package by.btslogistics.beltamozhservisproject.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+@Service
+public class SaveFileService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SaveFileService.class);
+
+    public void saveJavaFile(List<String> javaFileList, File updatedFile) {
+        try {
+            LOGGER.info("\nstaring save new file");
+            File file = new File(String.valueOf(updatedFile));
+            Files.write(Path.of(String.valueOf(updatedFile)), javaFileList);
+            LOGGER.info("file saved!");
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+    }
+}
