475337363be564f008fdeda2f831bc56838dcf8e
y.shtykh <LopkA_02>
2023-01-16 15:42:09 +0300

    bug fix

--- src/main/java/by/btslogistics/beltamozhservisproject/service/ChangeCodeCheckService.java
+++ src/main/java/by/btslogistics/beltamozhservisproject/service/ChangeCodeCheckService.java
@@ -9,9 +9,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 @Service
@@ -24,30 +22,42 @@ public class ChangeCodeCheckService {
 
     public void findCodeChecks(File excelFile, File javaCode, File updatedFile) throws IOException {
         String str = "-";
+        int count = 0;
         List<String> file = Files.readAllLines(Path.of(String.valueOf(javaCode)));
         Map<String, String> checks = ExcelService.oldAndNewChecksFromExcel(excelFile);
         for (Map.Entry<String, String> check: checks.entrySet()) {
             LOGGER.info(check.getKey() + " : " + check.getValue());
         }
-        LOGGER.info(str.repeat(35) + " : checks for replace: " + checks.size() + "\n");
+        for (Map.Entry<String, String> check : checks.entrySet()) {
+            if (!check.getKey().equals(check.getValue()))
+                count++;
+        }
+        LOGGER.info(str.repeat(35) + " : checks for replace: " + checks.size());
+        LOGGER.info(str.repeat(35) + " : same code_checks: " + count  + "\n");
+        count = 0;
         replaceCodeCheck(checks, file, updatedFile);
     }
 
     private void replaceCodeCheck(Map<String, String> checks, List<String> file, File updatedFile) {
         List<String> newFile = new ArrayList<>();
         AtomicBoolean fl = new AtomicBoolean(true);
-        file.forEach(line -> {
+        String k = null;
+        String v = null;
+        for (String line : file) {
             for (Map.Entry<String, String> check : checks.entrySet()) {
                 if (line.contains(check.getKey())) {
                     newFile.add(line.replace(line.split("\"")[1], check.getValue()));
                     fl.set(false);
                     LOGGER.info("find check for replace: " + check.getKey() + " replacing on: " + check.getValue());
+                    k = check.getKey();
+                    v = check.getValue();
                 }
             }
-            if (fl.getAndSet(true)) {
+            if (k != null && v != null)
+                checks.remove(k, v);
+            if (fl.getAndSet(true))
                 newFile.add(line);
-            }
-        });
+        }
         saveFileService.saveJavaFile(newFile, updatedFile);
     }
 
